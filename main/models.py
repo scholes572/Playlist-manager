6c9417572453fd47c61f81cd1ecb5daa5549685f
Agostino <agostinoscholes572@gmail.com>
2025-08-27 12:15:40 +0300

    Add artist model with relationship to album and song

--- main/models.py
+++ main/models.py
@@ -3,3 +3,12 @@ from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 Base = declarative_base()
 
+class Artist(Base):
+    __tablename__ = "artist"
+    id = column(Interger, primary_key=True)
+    name = column(String(100), nullable=False)
+    genre = column(String(50))
+    debut_year = column(Interger)
+
+    albums = relationship("Album", back_populates="artist")
+    songs = relationship("Song", back_populates="album")
