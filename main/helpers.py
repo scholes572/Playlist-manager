00390c2eb6d292dca275ccd19dd573f0651d7c75
Agostino <agostinoscholes572@gmail.com>
2025-08-27 23:42:18 +0300

    Add CRUD functions to create artist, album and song

--- main/helpers.py
+++ main/helpers.py
@@ -1,4 +1,19 @@
 from models import session, Artist, Album, Song
 
+def add_artist(name, genre, debut_year):
+    new_artist = Artist(name=name, genre=genre, debut_year=debut_year)
+    session.add(new_artist)
+    session.commit()
+
+def add_album(title, release_year, artist_id):
+    new_album = Album(title=title, release_year=release_year, artist_id=artist_id)
+    session.add(new_album)
+    session.commit()
+
+def add_song(title,duration, album_id):
+    new_song = Song(title=title, duration=duration, album_id=album_id)
+    session.add(new_song)
+    session.commit()
+        
 
   
