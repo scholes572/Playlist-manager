2ffb886c0f4088b8395d26c086bf964bb39ffeef
Agostino <agostinoscholes572@gmail.com>
2025-08-27 14:44:33 +0300

    Add helper function to create and save an album

--- main/helpers.py
+++ main/helpers.py
@@ -1,5 +1,6 @@
 from models import session, Artist, Album, Song
 
+#All artists helpers
 def add_artist(name, genre=None, debut_year=None):
     artist = Artist(name=name, genre=genre, debut_year=debut_year)
     session.add(artist)
@@ -9,3 +10,10 @@ def add_artist(name, genre=None, debut_year=None):
 def list_artists():
     return session.query(Artist).all()
 
+
+#All album helpers
+def add_album(title, release_year, artist_id):
+    album = Album(title=title, release_year=release_year, artist_id=artist_id)
+    session.add(album)
+    session.commit()
+    return album
