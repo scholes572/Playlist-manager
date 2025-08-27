a86010066d65cb4190fb13e8c7cd0d7fd9ab959a
Agostino <agostinoscholes572@gmail.com>
2025-08-27 23:50:47 +0300

    Add functions to get albums by artist and songs by album

--- main/helpers.py
+++ main/helpers.py
@@ -16,9 +16,28 @@ def add_song(title,duration, album_id):
     session.commit()
         
 
+
 def list_artists():
     return session.query(Artist).all()
+
 def list_albums():
     return session.query(Album).all()
+
 def list_songs():
     return session.query(Song).all()  
+
+
+
+def get_albums_by_artist(artist_id):
+    """Return all albums for a given artist ID"""
+    artist = session.query(Artist).get(artist_id)
+    if artist:
+        return artist.albums
+    return None
+
+def get_songs_by_album(album_id):
+    """Return all songs for a givem album ID"""
+    album = session.query(Album).get(album_id)
+    if album:
+        return album.songs
+    return None
