f634678562926166625833e4079e3be30eb9abd2
Agostino <agostinoscholes572@gmail.com>
2025-08-27 14:25:34 +0300

    Add helper function to create and save an artist

--- main/helpers.py
+++ main/helpers.py
@@ -1,2 +1,7 @@
 from models import session, Artist, Album, Song
 
+def add_artist(name, genre=None, debut_year=None):
+    artist = Artist(name=name, genre=genre, debut_year=debut_year)
+    session.add(artist)
+    session.commit()
+    return artist
