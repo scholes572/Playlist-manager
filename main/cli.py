f684691469454fa5167c55ab4b25fdec6ec1c949
Agostino <agostinoscholes572@gmail.com>
2025-08-27 15:45:59 +0300

    Add main() function with music databae CLI menu

--- main/cli.py
+++ main/cli.py
@@ -1,1 +1,12 @@
 from helpers import add_artist, list_artists, add_album, list_albums, add_song, list_songs
+
+def main():
+    while True:
+        print("\n🎵 Music Database CLI 🎵")
+        print("1. Add Artist")
+        print("2. List Artists")
+        print("3. Add Album")
+        print("4. List Albums")
+        print("5. Add Song")
+        print("6. List Songs")
+        print("0. Exit")
